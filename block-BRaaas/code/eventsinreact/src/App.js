b4cb8c077a5d4b5a32e2f826c11e1a51236fed63
oshingate <oshingate@gmail.com>
2021-07-05 13:48:40 +0530

    Assignment : events in react

---
+++ block-BRaaas/code/eventsinreact/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import Hero from './components/Hero';
+import Help from './components/Help';
+import Greet from './components/Greet';
+import Fruite from './components/Fruite';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Hero />
+      <Help />
+      <Greet />
+      <Fruite />
+    </div>
+  );
+}
+
+export default App;
